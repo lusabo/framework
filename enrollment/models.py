0be3a255cd37fff31a75114d20e6f11e561f53d9
Luciano Borges <lucianosantosborges@gmail.com>
2025-01-18 14:41:50 +0000

    Criação das migrations e dados para popular o banco de dados

--- enrollment/models.py
+++ enrollment/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Course(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
 class Student(models.Model):
 
     GENDER_CHOICES = [
@@ -7,32 +13,6 @@ class Student(models.Model):
         ("F", "Feminino"),
         ("O", "Outro")
     ]
-
-    name = models.CharField(max_length=100)
-    date_birth = models.DateField()
-    gender = models.CharField(
-        max_length=10,
-        choices=GENDER_CHOICES
-    )
-
-    def __str__(self):
-        return self.name
-
-class Course(models.Model):
-    description = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.description
-
-class Enrollment(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    semester = models.CharField(max_length=50, help_text="Ex.: 2025.1")
-
-    def __str__(self):
-        return f"{self.student.name} - {self.course.description} - {self.semester}"
-
-class Socioeconomic(models.Model):
 
     MARITAL_STATUS_CHOICES = [
         ("Solteiro", "Solteiro"),
@@ -44,26 +24,43 @@ class Socioeconomic(models.Model):
     TRANSPORTATION_CHOICES = [
         ("Carro", "Carro"),
         ("Transporte público", "Transporte público"),
-        ("Caminhada", "Caminhada"),
+        ("A pé", "A pé"),
         ("Bicicleta", "Bicicleta"),
         ("Outros", "Outros"),
     ]
 
-    enrollment = models.OneToOneField("Enrollment", on_delete=models.CASCADE)
+    DEFAULT_RISK = [
+        ("Baixo", "Baixo"),
+        ("Médio", "Médio"),
+        ("Alto", "Alto"),
+    ]
+
+    name = models.CharField(max_length=100)
+    date_birth = models.DateField()
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES
+    )
+    enrollment_number = models.CharField(max_length=10)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     marital_status = models.CharField(
-        max_length=50,
+        max_length=15,
         choices=MARITAL_STATUS_CHOICES
     )
     children = models.BooleanField(default=False)
     disabled = models.BooleanField(default=False)
     mode_transportation = models.CharField(
-        max_length=50,
+        max_length=30,
         choices=TRANSPORTATION_CHOICES
     )
     household_size = models.PositiveIntegerField()
     annual_household_income = models.FloatField()
+    defaulter = models.BooleanField(default=False)
+    default_risk = models.CharField(
+        max_length=5,
+        choices=DEFAULT_RISK,
+        default="Baixo"
+    )
 
     def __str__(self):
-        return f"Socioeconomic for {self.enrollment}"
-
-
+        return self.name
